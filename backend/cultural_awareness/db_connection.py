36f4ad694955fd381bcead68f4a0e5b3849f3ac8
freeman91 <addisonfreeman91@gmail.com>
2020-10-07 00:40:09 -0400

    Update to use docker env variables, mongo container IP

--- backend/cultural_awareness/db_connection.py
+++ backend/cultural_awareness/db_connection.py
@@ -1,28 +1,47 @@
 import os
 from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
-from pathlib import Path
-from dotenv import load_dotenv
-dotenv_path = Path('../.env')
-load_dotenv(dotenv_path=dotenv_path)
+from pymongo.errors import ServerSelectionTimeoutError
+
+#TODO: DOMAIN should be dynamically generated based on mongo container IP address
+DOMAIN = '172.24.0.2'
+PORT = '27017'
+MONGO_INITDB_DATABASE = os.getenv("MONGO_INITDB_DATABASE")
+MONGO_INITDB_ROOT_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+MONGO_INITDB_ROOT_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
 
 
 def connect():
     """
     establishes a connection to the database
-    requires MONGO_URI, DATABASE in .env file
+    
+    ENV variables:
+        DOMAIN - container IP address of the mongodb container
+        PORT - port expose on the mongodb container
+        MONGO_INITDB_DATABASE - database name
+        MONGO_INITDB_ROOT_USERNAME - root access username
+        MONGO_INITDB_ROOT_PASSWORD - root access user password
 
     Returns:
-        a database instance from the mongodb instance running on MONGO_URI
-        with the name DATABASE
+        a database instance with name MONGO_INITDB_DATABASE
 
     Raises:
-        an ConnectionFailure when it cant connect to the database
+        a ServerSelectionTimeoutError when connection takes more than 3 seconds
     """
-    client = MongoClient(os.getenv("MONGO_URI"))
     try:
-        client.admin.command('ismaster')
-    except ConnectionFailure:
-        print("Could not connect to server")
+        # try to instantiate a client instance
+        client = MongoClient(
+            host=[DOMAIN + ":" + PORT],
+            serverSelectionTimeoutMS=5000,  # 3 second timeout
+            username=MONGO_INITDB_ROOT_USERNAME,
+            password=MONGO_INITDB_ROOT_PASSWORD)
 
-    return client[os.getenv("DATABASE")]
+        # print the version of MongoDB server if connection successful
+        print("server version:", client.server_info()["version"])
+
+        return client[MONGO_INITDB_DATABASE]
+
+    except ServerSelectionTimeoutError as err:
+        client = None
+
+        # catch pymongo.errors.ServerSelectionTimeoutError
+        print("pymongo ERROR:", err)
