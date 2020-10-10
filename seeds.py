783355a8fb46f90ce491d877a60b4a48c9a494e0
freeman91 <addisonfreeman91@gmail.com>
2020-10-10 01:00:31 -0400

    replace example-data with seeds file

---
+++ seeds.py
@@ -0,0 +1,101 @@
+african_americans = {
+  "name": "African Americans",
+  "general_insights": [
+    {
+      "text": "It’s helpful to make a distinction in terminology – “blacks” can be defined as all persons of African descent, whose genealogical connection is to Africa, and in particular West Africa. However, because skin color and culture are not the same thing, when discussing cultural beliefs and practices among this group, the term African American best refers to “blacks” in the United States, people whose sociocultural roots are in the North American experience, but who are of African descent.",
+      "source": "http://www.dimensionsofculture.com/2011/05/health-care-for-african-american-patientsfamilies/",
+    },
+    {
+      "text": "Older African Americans may be suspicious of clinicians, because of experiences of past generations of African Americans with health care.",
+      "source": "http://www.dimensionsofculture.com/2011/05/health-care-for-african-american-patientsfamilies/",
+    },
+    {
+      "text": "Generally speaking, in the African American experience, spirituality is a fundamental part of how many people process and reconcile the experience of death. African Americans tend to believe in the sanctity of life and rely on a strong sense of community and family at times of loss. Family-centered consensus is valued in decision-making and there’s often a strong need for extended family to gather at a time of death; the family should be informed of an impending death so that extended family members who live out of state can be notified.",
+      "source": "http://www.dimensionsofculture.com/2011/05/health-care-for-african-american-patientsfamilies/",
+    },
+    {
+      "text": "African American families may refuse service because they do not believe they need it, in spite of high levels of stress.",
+      "source": "https://www.healthnetworksolutions.net/index.php/understanding-different-cultures#Black",
+    },
+    {
+      "text": "African American families are typically very private, not sharing concerns with strangers.",
+      "source": "https://www.healthnetworksolutions.net/index.php/understanding-different-cultures#Black",
+    },
+    {
+      "text": "African American families may not be aware of care options/community resources, or there are barriers to access.",
+      "source": "https://www.healthnetworksolutions.net/index.php/understanding-different-cultures#Black",
+    },
+    {
+      "text": "Blacks/African Americans face increased incidence of disease, including heart disease, stroke, kidney disease, diabetes, vascular dementia and Alzheimer's disease.",
+      "source": "https://www.healthnetworksolutions.net/index.php/understanding-different-cultures#Black",
+    },
+    {
+      "text": "Family caregivers may lack trust in service providers and this can show in response.  Time is necessary for building a trusting relationship.",
+      "source": "https://www.healthnetworksolutions.net/index.php/understanding-different-cultures#Black",
+    },
+    {
+      "text": "Important to address elders with titles such as 'Mrs' or 'Sir'.",
+      "source": "https://www.healthnetworksolutions.net/index.php/understanding-different-cultures#Black",
+    },
+  ],
+  "specilaized_insights": {
+    "heart_disease": [
+      {
+        "text": "African Americans ages 18-49 are 2 times as likely to die from heart disease than whites.",
+        "source": "https://www.cdc.gov/vitalsigns/aahealth/index.html",
+      },
+    ],
+    "blood_pressure": [
+      {
+        "text": "African Americans ages 35-64 years are 50% more likely to have high blood pressure than whites.",
+        "source": "https://www.cdc.gov/vitalsigns/aahealth/index.html",
+      },
+    ],
+  }
+}
+
+jehovahs_witnesses = {
+  "name": "Jehovah's Witnesses",
+  "general_insights": [
+    {
+      "text": "A majority of Jehovah’s Witnesses believe that it is against God’s will to receive blood and therefore they refuse blood donations, often even if it is their own blood",
+      "source": "https://www.medicalprotection.org/southafrica/casebook/casebook-may-2014/the-challenges-of-treating-jehovah's-witnesses",
+    },
+    {
+      "text": "The vast majority of medical treatments do not conflict with Jehovah's Witnesses beliefs. Therefore, personal choice is involved. One Witness might decide to accept a particular medicine or treatment, while another Witness might reject that same treatment",
+      "source": "https://www.jw.org/en/jehovahs-witnesses/faq/jehovahs-witnesses-medical-treatment/",
+    },
+    {
+      "text": "It is important that each Jehovah's Witness patient is asked in private what information may be passed on to relatives. If a patient elects to receive a ‘forbidden’ blood product there could be grave social implications if that decision becomes known.",
+      "source": "https://www.nursingtimes.net/clinical-archive/haematology/nursing-with-dignity-part-9-jehovahs-witnesses-23-04-2002/",
+    },
+  ],
+  "specilaized_insights": {
+    "abortion": [
+      {
+        "text": "Jehovah’s Witnesses see the elective termination of pregnancy as the wilful taking of human life",
+        "source": "https://www.nursingtimes.net/clinical-archive/haematology/nursing-with-dignity-part-9-jehovahs-witnesses-23-04-2002/",
+      },
+    ],
+    "birth_control": [
+      {
+        "text": "Birth control methods that effectively terminate a pregnancy are avoided by Jehovah's Witnesses",
+        "source": "https://www.nursingtimes.net/clinical-archive/haematology/nursing-with-dignity-part-9-jehovahs-witnesses-23-04-2002/",
+      },
+    ],
+    "reproductive_technology": [
+      {
+        "text": "In vitro fertilisation involving eggs and sperm from unmarried people is unacceptable as it is considered adultery, as is gestational surrogacy",
+        "source": "https://www.nursingtimes.net/clinical-archive/haematology/nursing-with-dignity-part-9-jehovahs-witnesses-23-04-2002/",
+      },
+    ],
+  }
+}
+
+admins = [
+  {
+    "name": "John Doe",
+    "email": "JohnDoe@example.com",
+    "password": "password",
+  },
+]
