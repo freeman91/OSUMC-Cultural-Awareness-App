3003a6eca1cb726228ae412ec7ca3606c394524c
freeman91 <addisonfreeman91@gmail.com>
2020-10-10 01:30:31 -0400

    revert scripts

--- script/restore_dev_db.sh
+++
@@ -1,4 +0,0 @@
-# requires .env file and mongo container to be running
-
-source .env
-docker exec mongodb bash -c "mongorestore --archive='appdata/db-backup' --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD"
