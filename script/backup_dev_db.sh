5fb3d44737c719d0b6cc58ad63d7c3d1b1258bac
freeman91 <addisonfreeman91@gmail.com>
2020-10-10 12:57:10 -0400

    refactor db scripts

--- script/backup_dev_db.sh
+++ script/backup_dev_db.sh
@@ -1,4 +1,4 @@
-# requires .env file and mongo container to be running
+#!/bin/bash
 
 source .env
-docker exec mongodb bash -c "mongodump --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --archive='appdata/db-backup'"
+docker exec mongodb bash -c "mongodump --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --archive='appdata/db-backup.bak'"
--- script/restore_dev_db.sh
+++ script/restore_dev_db.sh
@@ -1,4 +1,4 @@
-# requires .env file and mongo container to be running
+#!/bin/bash
 
 source .env
-docker exec mongodb bash -c "mongorestore --archive='appdata/db-backup' --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD"
+docker exec mongodb bash -c "mongorestore --archive='appdata/db-backup.bak' --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD"
