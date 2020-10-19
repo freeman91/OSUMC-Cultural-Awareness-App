edd3de465ab0c276e3725311c18d6ba97c2abaf7
claus44 <claus.44@gmail.com>
2020-10-19 16:51:20 -0400

    initialized react navagation

--- App.tsx
+++ App.tsx
@@ -1,162 +1,178 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+//import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+//import { View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function adminFunctions() {
+      return (
+            <View style={styles.container}>
+            <View><Text style={{fontSize: 30, height: 50}}>Add new culture group</Text>
+            <Text>Name of Culture Group:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>General insight for culture group:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Specialized insight for culture group:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Submit </Text>
+                  </TouchableOpacity>
+                  
+            <View><Text style={{fontSize: 30, height: 50}}>Edit Culture Group</Text>
+            <Text>Name of Culture Group:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>General insight for culture group:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Specialized insight for culture group:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Submit </Text>
+                  </TouchableOpacity>
+            </View>
+
+            <View><Text style={{fontSize: 30, height: 50}}>Delete culture group</Text>
+                  <Text>Name of culture group:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Delete </Text>
+                  </TouchableOpacity>
+            </View>
+
+            <View><Text style={{fontSize: 30, height: 50}}>New Admin Account</Text>
+            <Text>Name:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Email:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Password:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Confirm Password:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Submit </Text>
+                  </TouchableOpacity>
+            </View>
+
+            <View><Text style={{fontSize: 30, height: 50}}>Show List of Admins</Text>
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Show List </Text>
+                  </TouchableOpacity>
+            </View>
+
+
+            <View><Text style={{fontSize: 30, height: 50}}>Invite New Admin</Text>
+                  <Text>Email:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Submit </Text>
+                  </TouchableOpacity>
+            </View>
+
+            <View><Text style={{fontSize: 30, height: 50}}>Update Admin Account</Text>
+            <Text>Name:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Email:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Password:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <Text>Confirm Password:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Submit </Text>
+                  </TouchableOpacity>
+            </View>
+
+            <View><Text style={{fontSize: 30, height: 50}}>Delete Admin Account</Text>
+                  <Text>Email:</Text>
+            <TextInput
+                  style={{height: 40}}
+                  placeholder="Type here!"
+                  />
+                  <TouchableOpacity
+                  style = {styles.submitButton}>
+                  <Text style = {styles.submitButtonText}> Delete </Text>
+                  </TouchableOpacity>
+            </View>
+
+
+
+      </View>
+
+
+
+      <View style={styles.container}>
+      </View>
+            <StatusBar style="auto" />
+      </View>
+      );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View><Text style={{fontSize: 30, height: 50}}>Add new culture group</Text>
-      <Text>Name of Culture Group:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>General insight for culture group:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Specialized insight for culture group:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Submit </Text>
-            </TouchableOpacity>
-            
-      <View><Text style={{fontSize: 30, height: 50}}>Edit Culture Group</Text>
-      <Text>Name of Culture Group:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>General insight for culture group:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Specialized insight for culture group:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Submit </Text>
-            </TouchableOpacity>
-      </View>
-
-      <View><Text style={{fontSize: 30, height: 50}}>Delete culture group</Text>
-            <Text>Name of culture group:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Delete </Text>
-            </TouchableOpacity>
-      </View>
-
-      <View><Text style={{fontSize: 30, height: 50}}>New Admin Account</Text>
-      <Text>Name:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Email:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Password:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Confirm Password:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Submit </Text>
-            </TouchableOpacity>
-      </View>
-
-      <View><Text style={{fontSize: 30, height: 50}}>Show List of Admins</Text>
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Show List </Text>
-            </TouchableOpacity>
-      </View>
-
-
-      <View><Text style={{fontSize: 30, height: 50}}>Invite New Admin</Text>
-            <Text>Email:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Submit </Text>
-            </TouchableOpacity>
-      </View>
-
-      <View><Text style={{fontSize: 30, height: 50}}>Update Admin Account</Text>
-      <Text>Name:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Email:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Password:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <Text>Confirm Password:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Submit </Text>
-            </TouchableOpacity>
-      </View>
-
-      <View><Text style={{fontSize: 30, height: 50}}>Delete Admin Account</Text>
-            <Text>Email:</Text>
-      <TextInput
-            style={{height: 40}}
-            placeholder="Type here!"
-            />
-            <TouchableOpacity
-               style = {styles.submitButton}>
-               <Text style = {styles.submitButtonText}> Delete </Text>
-            </TouchableOpacity>
-      </View>
-
-
-
-    </View>
-
-
-
-    <View style={styles.container}>
-    </View>
-      <StatusBar style="auto" />
-    </View>
-    
+      <NavigationContainer>{
+            <Stack.Navigator initialRouteName="adminFunctions">
+                  <Stack.Screen name="adminFunctions" component={adminFunctions} />
+            </Stack.Navigator>
+    }</NavigationContainer>
   );
 }
 
