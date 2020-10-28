ae66b58e29344d7f4be20f52d3899963286e8258
claus44 <claus.44@gmail.com>
2020-10-28 16:49:42 -0400

    forgot app.tsx

--- App.tsx
+++ App.tsx
@@ -7,117 +7,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useState } from "react";
 import Constants from 'expo-constants';
+import {homePage} from './src/views/homePage';
 
-function homePage({ navigation }) {
-      const [cultureGroup, setCultureGroup] = useState("");
-      const [delCultureGroup, deleteCultureGroup] = useState("");
-      const [cultureGenData, setGenCultureData] = useState("");
-      const [cultureSpecData, setCultureSpecData] = useState("");
-  
-      const handleDisclaimer = (evt) => {
-            alert('Pressed!');
-        }
-      
-        const handleAdminLogin = (evt) => {
-            alert('Pressed!')
-      }
 
-      const EmptyListMessage = ({ item }) => {
-            return (
-              // Flat List Item
-              <Text style={styles.emptyListStyle} onPress={() => getItem(item)}>
-                No Data Found
-              </Text>
-            );
-          };
-        
-          const ItemView = ({ item }) => {
-            return (
-              // Flat List Item
-              <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-                {item.id}
-                {'.'}
-                {item.title.toUpperCase()}
-              </Text>
-            );
-          };
-        
-          const ItemSeparatorView = () => {
-            return (
-              // Flat List Item Separator
-              <View
-                style={{
-                  height: 0.5,
-                  width: '100%',
-                  backgroundColor: '#C8C8C8',
-                }}
-              />
-            );
-          };
-        
-          const ListHeader = () => {
-            //View to set in Header
-            return (
-              <View style={styles.headerFooterStyle}>
-                <Text style={styles.textStyle}>Cultural Awareness Home Page</Text>
-              </View>
-            );
-          };
-        
-          const ListFooter = () => {
-            //View to set in Footer
-            return (
-              <View style={styles.bottomFooterStyle}>
-                <Button title="General Disclaimer" style="margin:50%" onPress={handleDisclaimer} />
-                <TouchableOpacity style={styles.btn} onPress={handleAdminLogin}>
-                <Image source={require('./assets/admin_login.png')}  style={styles.img}/>
-                </TouchableOpacity>
-              </View>
-            );
-          };
-        
-          const getItem = (item) => {
-            // Function for click on an item
-            alert('Id : ' + item.id + ' Title : ' + item.title);
-          };
-
-          const [dataSource, setDataSource] = useState([
-            { id: 1, title: 'Button' },
-            { id: 2, title: 'Card' },
-            { id: 3, title: 'Input' },
-            { id: 4, title: 'Avatar' },
-            { id: 5, title: 'CheckBox' },
-            { id: 6, title: 'Header' },
-            { id: 7, title: 'Icon' },
-            { id: 8, title: 'Lists' },
-            { id: 9, title: 'Rating' },
-            { id: 10, title: 'Pricing' },
-            { id: 11, title: 'Avatar' },
-            { id: 12, title: 'CheckBox' },
-            { id: 13, title: 'Header' },
-            { id: 14, title: 'Icon' },
-            { id: 15, title: 'Lists' },
-            { id: 16, title: 'Rating' },
-            { id: 17, title: 'Pricing' },
-          ]);
-
-      return (
-            <SafeAreaView style={{ flex: 1 }}>
-              <FlatList
-                data={dataSource} 
-                keyExtractor={(item, index) => index.toString()}
-                ItemSeparatorComponent={ItemSeparatorView}
-                //Header to show above listview
-                ListHeaderComponent={ListHeader}
-                //Footer to show below listview
-                ListFooterComponent={ListFooter}
-                renderItem={ItemView}
-                ListEmptyComponent={EmptyListMessage}
-              />
-            </SafeAreaView>
-        
-      );
-}
 
 function cultureGeneral({ navigation }) {
       return (
@@ -156,12 +48,21 @@ function cultureSpecialized({ navigation }) {
 }
 
 function adminLogin({ navigation }) {
+
+      // const getItem = (item) => {
+      //       // Function for click on an item
+      //       alert('Id : ' + item.id + ' Title : ' + item.title);
+      // };
+
       return (
             <View style={styles.container}>
                   
+                  <Text>Login</Text>
+                  <TextInput placeholder="Email Address"></TextInput>
+                  <TextInput placeholder="Password"></TextInput>
 
                   <Button
-                  title="Manage Culture Data"
+                  title="Login"
                   onPress={() => navigation.navigate('homePage')}
                   />
 
