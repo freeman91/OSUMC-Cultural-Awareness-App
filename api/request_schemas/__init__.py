5de335272b14ea1ac73a97f1efb687578d8a080c
freeman91 <addisonfreeman91@gmail.com>
2020-10-28 22:32:24 -0400

    Add comments

--- api/request_schemas/__init__.py
+++ api/request_schemas/__init__.py
@@ -2,21 +2,33 @@ from marshmallow import Schema, fields, ValidationError
 
 
 class CultureCreateSchema(Schema):
+    """
+    POST /vi/culture
+    """
     name = fields.String(required=True)
 
 
 class CultureUpdateSchema(Schema):
+    """
+    PUT /v1/culture/<name>
+    """
     name = fields.String(required=True)
     general_insights = fields.List(fields.Dict, required=True)
     specialized_insights = fields.List(fields.Dict, required=True)
 
 
 class AdminLoginSchema(Schema):
+    """
+    POST /v1/login
+    """
     email = fields.String(required=True)
     password = fields.String(required=True)
 
 
 class AdminRegisterSchema(Schema):
+    """
+    POST /v1/register
+    """
     name = fields.String(required=True)
     email = fields.String(required=True)
     password = fields.String(required=True)
@@ -24,10 +36,16 @@ class AdminRegisterSchema(Schema):
 
 
 class AdminInviteSchema(Schema):
+    """
+    POST /v1/admin/invite
+    """
     email = fields.String(required=True)
 
 
 class AdminUpdateSchema(Schema):
+    """
+    PUT /v1/admin/<email>
+    """
     email = fields.String(required=True)
     name = fields.String()
     password = fields.String()
@@ -36,6 +54,9 @@ class AdminUpdateSchema(Schema):
 
 
 def validate_request_body(schema, body):
+    """
+    Validates a request body using the corresponding request schema
+    """
     try:
         return schema().load(body)
     except ValidationError as err:
