5cfa0d9b2570fa06d11d0b0ec5b9e9f2503b9c93
Nick Hackman <snickhackman@gmail.com>
2020-11-07 11:15:43 -0500

    WIP

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,5 +1,7 @@
 import "react-native-gesture-handler";
-import * as React from "react";
+import React from "react";
+
+import { Provider as PaperProvider, Button, Avatar } from "react-native-paper";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -10,26 +12,36 @@ import { adminLogin } from "./views/adminLogin";
 import { adminRegistration } from "./views/adminRegistration";
 import { CultureView } from "./views/culture";
 import { Routes } from "./routes";
+import { Theme } from "./constants";
 
 function App() {
   const Stack = createStackNavigator<Routes>();
 
   return (
-    <NavigationContainer>
-      {
-        <Stack.Navigator initialRouteName="Culture">
-          <Stack.Screen
-            name="Culture"
-            component={CultureView}
-            initialParams={{ cultureName: "African Americans" }}
-          />
-          <Stack.Screen name="Home" component={homePage} />
-          <Stack.Screen name="Dashboard" component={adminDashboard} />
-          <Stack.Screen name="Login" component={adminLogin} />
-          <Stack.Screen name="Register" component={adminRegistration} />
-        </Stack.Navigator>
-      }
-    </NavigationContainer>
+    <PaperProvider theme={Theme}>
+      <NavigationContainer>
+        {
+          <Stack.Navigator initialRouteName="Culture">
+            <Stack.Screen
+              name="Culture"
+              component={CultureView}
+              initialParams={{ cultureName: "African Americans" }}
+              options={{
+                headerRight: () => (
+                  <Button onPress={() => console.log("button pressed")}>
+                    <Avatar.Text size={36} label="NH" />
+                  </Button>
+                ),
+              }}
+            />
+            <Stack.Screen name="Home" component={homePage} />
+            <Stack.Screen name="Dashboard" component={adminDashboard} />
+            <Stack.Screen name="Login" component={adminLogin} />
+            <Stack.Screen name="Register" component={adminRegistration} />
+          </Stack.Navigator>
+        }
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
