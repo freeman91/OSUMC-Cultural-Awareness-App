692533fa662e5cfbe5114434b1288a579c6e15f0
freeman91 <addisonfreeman91@gmail.com>
2020-11-09 19:42:04 -0500

    init login/register form

--- frontend/views/adminLogin.tsx
+++ frontend/views/adminLogin.tsx
@@ -1,40 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import 'react-native-gesture-handler';
 import * as React from 'react';
+import { ImageBackground, ScrollView, StyleSheet } from 'react-native';
+import { Button, Card, TextInput } from 'react-native-paper';
+
 
 const styles = StyleSheet.create({
+    card: {
+        marginTop: 50,
+        marginLeft: 10,
+        marginRight: 10,
+    },
     container: {
-      padding: 10,
-      fontSize: 18,
-      textAlign: 'center',
-    }
+        padding: 10,
+        fontSize: 18,
+    },
+    image: {
+        flex: 1,
+        justifyContent: "center"
+    },
 });
 
-export function adminLogin({ navigation }) {
+export function adminLogin({ route, navigation }) {
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [passwordConfirmation, setPasswordConfirmation] = React.useState('');
+    const bgImage = {
+        uri:
+          'https://images.pexels.com/photos/3137064/pexels-photo-3137064.jpeg',
+      };
 
-    // const getItem = (item) => {
-    //       // Function for click on an item
-    //       alert('Id : ' + item.id + ' Title : ' + item.title);
-    // };
+    const handleLogin = () => {
+        console.log('email: ', email)
+        console.log('password: ', password)
+    }
+
+    console.log('route: ', route);
 
     return (
-          <View style={styles.container}>
-                
-                <Text>Login</Text>
-                <TextInput placeholder="Email Address"></TextInput>
-                <TextInput placeholder="Password"></TextInput>
-
-                <Button
-                title="Login"
-                onPress={() => navigation.navigate('Home Page')}
+        <ImageBackground source={bgImage} imageStyle={{resizeMode: 'stretch'}} style={styles.image}>
+        <ScrollView
+          keyboardShouldPersistTaps={'always'}
+          removeClippedSubviews={false}
+        >
+            
+            <Card style={styles.card}>
+                <TextInput
+                    style={styles.container}
+                    mode="outlined"
+                    label="email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
                 />
-
-
-          <View style={styles.container}>
-          </View>
-                <StatusBar style="auto" />
-          </View>
+                <TextInput
+                    style={styles.container}
+                    mode="outlined"
+                    label="password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                />
+                { route.name == "Register" ?
+                    <TextInput
+                        style={styles.container}
+                        mode="outlined"
+                        label="password confirmation"
+                        value={passwordConfirmation}
+                        onChangeText={text => setPasswordConfirmation(text)}
+                    /> : <></> }
+                <Button icon="login" mode="contained" onPress={() => handleLogin()}>
+                    submit
+                </Button>
+            </Card>
+          
+        </ScrollView>
+        </ImageBackground>
     );
 }
 
