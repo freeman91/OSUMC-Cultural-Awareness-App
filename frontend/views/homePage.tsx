53365c157b8b454535a145e39c5d76658173f6f9
Nick Hackman <snickhackman@gmail.com>
2020-11-12 14:12:39 -0500

    Fix: merge issues
    Updated Culture.list, formatting fixes.

--- frontend/views/homePage.tsx
+++ frontend/views/homePage.tsx
@@ -1,16 +1,30 @@
 //import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, ScrollView, FlatList, SafeAreaView, Image } from 'react-native';
-import 'react-native-gesture-handler';
-import * as React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  SafeAreaView,
+  Image,
+} from "react-native";
+import "react-native-gesture-handler";
+import * as React from "react";
 import { useState, useEffect } from "react";
-import { Culture } from "/Users/nicklamanna/Desktop/OSUMC-Cultural-Awareness-App/frontend/api/culture";
-import { List, ActivityIndicator, Colors, Button, TouchableRipple, FAB } from 'react-native-paper';
+import { Culture } from "../api/culture";
+import {
+  List,
+  ActivityIndicator,
+  Colors,
+  Button,
+  FAB,
+} from "react-native-paper";
 
 const styles = StyleSheet.create({
   emptyListStyle: {
     padding: 10,
     fontSize: 18,
-    textAlign: 'center',
+    textAlign: "center",
   },
   itemStyle: {
     padding: 10,
@@ -18,36 +32,34 @@ const styles = StyleSheet.create({
   img: {
     padding: 35,
     height: 70,
-    width: '25%'
+    width: "25%",
   },
   headerFooterStyle: {
-    width: '100%',
+    width: "100%",
     height: 45,
-    backgroundColor: '#606070',
+    backgroundColor: "#606070",
   },
   bottomFooterStyle: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
     height: 80,
-    backgroundColor: '#606070',
+    backgroundColor: "#606070",
   },
   textStyle: {
-    textAlign: 'center',
-    color: '#fff',
+    textAlign: "center",
+    color: "#fff",
     fontSize: 18,
     padding: 7,
   },
-  btn: {
-
-  },
+  btn: {},
   fab: {
-    position: 'absolute',
+    position: "absolute",
     margin: 16,
     right: 0,
-    bottom: 0
-  }
+    bottom: 0,
+  },
 });
 
 export function homePage({ navigation }) {
@@ -57,7 +69,7 @@ export function homePage({ navigation }) {
     const fetchData = async () => {
       let cultureNames = await Culture.list();
       setCultures(cultureNames);
-    }
+    };
 
     fetchData();
   }, []);
@@ -75,46 +87,58 @@ export function homePage({ navigation }) {
     //View to set in Footer
     return (
       <View style={styles.bottomFooterStyle}>
-        <Button onPress={() => console.log("Pressed!")} >General Disclaimer</Button>
+        <Button onPress={() => console.log("Pressed!")}>
+          General Disclaimer
+        </Button>
         <TouchableOpacity style={styles.btn} onPress={handleAdminLogin}>
-          <Image source={require('../assets/admin_login.png')} style={styles.img} />
+          <Image
+            source={require("../assets/admin_login.png")}
+            style={styles.img}
+          />
         </TouchableOpacity>
       </View>
     );
   };
 
   const handleAdminLogin = (evt) => {
-    navigation.navigate('Admin Login');
+    navigation.navigate("Admin Login");
     //alert('Pressed!')
-
-
-  }
+  };
   const handleDisclaimer = (evt) => {
-    alert('Pressed!');
-  }
+    alert("Pressed!");
+  };
 
   if (cultures === null)
-    return (<ActivityIndicator animating={true} color={Colors.red800} />)
+    return <ActivityIndicator animating={true} color={Colors.red800} />;
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FAB icon="plus" style={styles.fab}></FAB>
-      <FlatList style={{ flex: 1 }}
+      <FlatList
+        style={{ flex: 1 }}
         data={cultures}
         keyExtractor={(item, index) => index.toString()}
         //Footer to show below listview
         ListFooterComponent={ListFooter}
         renderItem={({ item }) => {
-          return (<TouchableOpacity onPress={() => console.log("Pressed!")}><List.Item
-            title={item}
-            right={props => <Button icon="delete" onPress={() => Culture.Delete(item, "")}> </Button>}
-          />
-          </TouchableOpacity>
-          )
+          return (
+            <TouchableOpacity onPress={() => console.log("Pressed!")}>
+              <List.Item
+                title={item.name}
+                right={(props) => (
+                  <Button
+                    icon="delete"
+                    onPress={() => Culture.delete(item.name, "")}
+                  >
+                    {" "}
+                  </Button>
+                )}
+              />
+            </TouchableOpacity>
+          );
         }}
-      //ListEmptyComponent={EmptyListMessage}
+        //ListEmptyComponent={EmptyListMessage}
       />
     </SafeAreaView>
-  )
+  );
 }
-
-
