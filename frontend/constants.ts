cd042d7aa6be3725dae92d9b69d52d2e3e8028ef
freeman91 <addisonfreeman91@gmail.com>
2020-11-22 16:22:01 -0500

    use ec2 instance ip address

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,6 +1,7 @@
 import { DefaultTheme } from "react-native-paper";
 
-export const API_URL = "http://localhost:5000/v1";
+// export const API_URL = "http://localhost:5000/v1";
+export const API_URL = "18.189.142.71:5000/v1";
 
 export const Theme = {
   ...DefaultTheme,
