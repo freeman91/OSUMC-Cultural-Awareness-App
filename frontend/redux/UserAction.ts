ad8286de124a8a8f8074898d581a327bf925ebba
Nick Hackman <snickhackman@gmail.com>
2020-11-24 20:12:07 -0500

    Feat(frontend): Redux action for logging out
    Called RESET_USER, probably better to switch the current one from
    UPDATE_USER to LOGIN_USER and then name this one LOGOUT_USER instead of
    their current names are more explicit.

--- frontend/redux/UserAction.ts
+++ frontend/redux/UserAction.ts
@@ -4,3 +4,8 @@ export const updateUser = (user: Store["user"]) => ({
   type: "UPDATE_USER",
   payload: user,
 });
+
+export const resetUser = () => ({
+  type: "RESET_USER",
+  payload: {},
+});
--- frontend/redux/index.ts
+++ frontend/redux/index.ts
@@ -2,10 +2,9 @@ import { combineReducers } from "redux";
 
 import { USER_INITIAL_STATE, userReducer } from "./UserReducer";
 import { THEME_INITIAL_STATE, themeReducer } from "./ThemeReducer";
-import { updateUser } from "./UserAction";
+import { updateUser, resetUser } from "./UserAction";
 import { updateTheme } from "./ThemeAction";
 
-import { Admin } from "../api/admin";
 import { ThemeType } from "../constants";
 
 export const Reducer = combineReducers({
@@ -13,6 +12,18 @@ export const Reducer = combineReducers({
   user: userReducer,
 });
 
-export type Store = { user: { user: Admin; token: string }; theme: ThemeType };
+export type Store = {
+  user: {
+    user: { name: string; email: string; superUser: boolean };
+    token: string;
+  };
+  theme: ThemeType;
+};
 
-export { USER_INITIAL_STATE, THEME_INITIAL_STATE, updateUser, updateTheme };
+export {
+  USER_INITIAL_STATE,
+  THEME_INITIAL_STATE,
+  updateUser,
+  resetUser,
+  updateTheme,
+};
