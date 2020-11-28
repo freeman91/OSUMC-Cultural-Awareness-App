5eb65383ebd4deae653e280c68196cee0d6010f2
freeman91 <addisonfreeman91@gmail.com>
2020-11-27 22:20:42 -0500

    add -e

--- script/restart-services.sh
+++ script/restart-services.sh
@@ -13,20 +13,20 @@ NC='\033[0m'
 
 if [ $1 == 0 ]
   then
-    echo "${RED}provide an ssh key as an argument${NC}"
+    echo -e "${RED}provide an ssh key as an argument${NC}"
     exit 1
 fi
 
-echo "${GREEN}Fetching latest master${NC}"
+echo -e "${GREEN}Fetching latest master${NC}"
 cd OSUMC-Cultural-Awareness-App/
 git fetch
 # change to master
 git pull origin deploy-script
 
-echo "${GREEN}Installing python packages${NC}"
+echo -e "${GREEN}Installing python packages${NC}"
 /home/ec2-user/.local/bin/pipenv install
 
-echo "${GREEN}Restarting services${NC}"
+echo -e "${GREEN}Restarting services${NC}"
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn 
 sudo systemctl restart nginx
