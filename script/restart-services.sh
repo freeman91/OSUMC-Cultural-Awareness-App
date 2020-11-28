9ae19ccc2bc2cc617f72ae8dfb7bd52a341b0025
freeman91 <addisonfreeman91@gmail.com>
2020-11-27 22:19:33 -0500

    fix printing in color

--- script/restart-services.sh
+++ script/restart-services.sh
@@ -11,22 +11,22 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 
-if [ $1 -eq 0 ]
+if [ $1 == 0 ]
   then
-    echo "{RED}provide an ssh key as an argument{NC}"
+    echo "${RED}provide an ssh key as an argument${NC}"
     exit 1
 fi
 
-echo "{GREEN}Fetching latest master{NC}"
+echo "${GREEN}Fetching latest master${NC}"
 cd OSUMC-Cultural-Awareness-App/
 git fetch
 # change to master
 git pull origin deploy-script
 
-echo "{GREEN}Installing python packages{NC}"
+echo "${GREEN}Installing python packages${NC}"
 /home/ec2-user/.local/bin/pipenv install
 
-echo "{GREEN}Restarting services{NC}"
+echo "${GREEN}Restarting services${NC}"
 sudo systemctl daemon-reload
 sudo systemctl restart gunicorn 
 sudo systemctl restart nginx
