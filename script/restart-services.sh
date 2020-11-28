cd5d15dee51b401d550791fca2255b2301f994cf
freeman91 <addisonfreeman91@gmail.com>
2020-11-27 22:17:03 -0500

    update script

--- script/restart-services.sh
+++ script/restart-services.sh
@@ -1,16 +1,32 @@
 #!/usr/bin/env bash
 
+# TODO
+# error checking on each command
+# how to pass ssh key to github actions
 
+set -e
+
+GREEN='\033[0;31m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+
+if [ $1 -eq 0 ]
+  then
+    echo "{RED}provide an ssh key as an argument{NC}"
+    exit 1
+fi
+
+echo "{GREEN}Fetching latest master{NC}"
 cd OSUMC-Cultural-Awareness-App/
 git fetch
-
 # change to master
 git pull origin deploy-script
 
+echo "{GREEN}Installing python packages{NC}"
 /home/ec2-user/.local/bin/pipenv install
 
+echo "{GREEN}Restarting services{NC}"
 sudo systemctl daemon-reload
-
 sudo systemctl restart gunicorn 
-
 sudo systemctl restart nginx
