bb3d23dd5ffb8e422d0e2804505508ac57a3ea8b
freeman91 <addisonfreeman91@gmail.com>
2020-11-28 10:49:02 -0500

    refactor

--- script/deploy_production_backend.sh
+++ script/deploy_production_backend.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-# ARG $1 - private key to ssh into ec2 instance
-
-
-ssh -i $1 ec2-user@18.189.142.71 OSUMC-Cultural-Awareness-App/script/restart-services.sh
-
-
+ssh -i $1 ec2-user@18.189.142.71 OSUMC-Cultural-Awareness-App/script/restart_services.sh
