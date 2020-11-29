ac7e6a0d1d22cfe4350bdc7b4e3a5c4ee9e75734
claus44 <claus.44@gmail.com>
2020-11-29 11:37:19 -0500

    adding home/admins.tsx

---
+++ frontend/views/Home/Admins.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { FlatList, Alert, View } from "react-native";
+
+import { List, IconButton, Modal, Portal, Text } from "react-native-paper";
+
+import { Admin } from "../../lib";
+
+/**
+ * Properties for {@link Admins}
+ */
+type AdminProps = {
+  token: string;
+  admins: Admin[];
+  // Refresh admins
+  onRefresh: () => void;
+};
+
+/**
+ * Component that displays a list of components of {@link Admin}
+ *
+ * @param {AdminProps} props
+ * @returns {React.ReactElement} React component
+ */
+export default function Admins(props: AdminProps): React.ReactElement {
+  const { token, admins, onRefresh } = props;
+  const [deleteModal, setDeleteModal] = useState(false);
+
+  const onDelete = async (email: string) => {
+    try {
+      await Admin.delete(email, token);
+    } catch {
+      // show error message
+    }
+    onRefresh();
+  };
+
+  const onEdit = (user: {
+    email: string;
+    name: string;
+    superUser: boolean;
+  }) => {
+    //TODO: update Admin.update() params
+    try {
+      //Admin.update(email, token)
+      onRefresh();
+    } catch {
+      // show error message
+    }
+  };
+
+  return (
+    <View>
+      <FlatList
+        style={{ flex: 1 }}
+        data={admins}
+        keyExtractor={(_, index) => index.toString()}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <List.Item
+                title={item.email}
+                onPress={() => {
+                  Alert.alert("user pressed", item.name, [
+                    { text: "OK", onPress: () => console.log("OK Pressed") },
+                  ]);
+                }}
+                right={() =>
+                  props.token !== "" && (
+                    <View style={{ flexDirection: "row" }}>
+                      <IconButton icon="pencil" onPress={() => onEdit(item)} />
+                      <IconButton
+                        icon="delete"
+                        onPress={() => onDelete(item.email)}
+                      />
+                    </View>
+                  )
+                }
+              />
+              <Portal>
+                <Modal
+                  visible={deleteModal}
+                  onDismiss={() => setDeleteModal(false)}
+                >
+                  <Text>Are you sure you want to delete {item.email}</Text>
+                  <IconButton
+                    icon="delete"
+                    onPress={() => onDelete(item.email)}
+                  />
+                </Modal>
+              </Portal>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+}
