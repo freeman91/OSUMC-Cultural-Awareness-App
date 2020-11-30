dd5382482a7278617f7f9fef4200873cd5640c81
freeman91 <addisonfreeman91@gmail.com>
2020-11-30 14:16:39 -0500

    add validation for Home forms

--- frontend/views/Home/validation.ts
+++ frontend/views/Home/validation.ts
@@ -3,7 +3,16 @@ import * as Yup from "yup";
 /**
  * Invite Email Validation Schema, a Yup Schema for basic validation
  */
-export default Yup.object().shape({
+export const EmailValidation = Yup.object().shape({
   // Email must be provided and be a valid email.
   email: Yup.string().email("Invalid email address").required("Required"),
+});
+
+/**
+ * Invite Email Validation Schema, a Yup Schema for basic validation
+ */
+export const EmailNameValidation = Yup.object().shape({
+  // Email must be provided and be a valid email.
+  email: Yup.string().email("Invalid email address").required("Required"),
+  name: Yup.string().required("Required"),
 });
